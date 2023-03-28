ef95aa9490a67c4d10c34142f3e7d27e9849e526
nelson <nelson3chege@gmail.com>
2023-03-28 23:01:49 +0300

    feat : basic data types
    - how do basic types work in typescript

--- sandbox.js
+++ sandbox.js
@@ -1,6 +1,22 @@
-var character = "mario";
-console.log(character);
-var inputs = document.querySelectorAll("input");
-inputs.forEach(function (input) {
-    console.log(input);
-});
+// variables are declared same as javascript
+//but once variables type is set it cannnot be changed
+// you dont have to specify the data type when declaring variables because typescript will infer it
+var character = "Mario";
+var age = 30;
+var isBlackBelt = false;
+//cannot
+// character = 30
+// can
+character = "Luigi";
+//cannot
+// age = 'Mario'
+// can
+age = 20;
+//cannot
+// isBlackBelt = 'yes'
+// can
+isBlackBelt = true;
+var cicl = function (diameter) {
+    return diameter * Math.PI;
+};
+console.log(cicl(7.5));
--- sandbox.ts
+++ sandbox.ts
@@ -1,6 +1,31 @@
-const character = "mario";
-console.log(character);
-const inputs = document.querySelectorAll("input");
-inputs.forEach(function (input) {
-  console.log(input);
-});
+// variables are declared same as javascript
+//but once variables type is set it cannnot be changed
+
+// you dont have to specify the data type when declaring variables because typescript will infer it
+let character = "Mario";
+let age = 30;
+let isBlackBelt = false;
+
+//cannot
+// character = 30
+
+// can
+character = "Luigi";
+
+//cannot
+// age = 'Mario'
+
+// can
+age = 20;
+
+//cannot
+// isBlackBelt = 'yes'
+
+// can
+isBlackBelt = true;
+
+const cicl = (diameter: number) => {
+  return diameter * Math.PI;
+};
+
+console.log(cicl(7.5));

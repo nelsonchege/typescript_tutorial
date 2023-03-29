7cbfcf8c90ae6751dfce4efbd6c62cd98be2e0ae
nelson chege <nelson3chege@gmail.com>
2023-03-29 10:53:07 +0300

    update:adding conditional value if non entered

--- public/sandbox.js
+++ public/sandbox.js
@@ -3,6 +3,14 @@ let sample_function;
 let greet = (a) => {
     console.log(`hello ${a}`);
 };
+let add = (a, b, c) => {
+    console.log(a + b);
+    console.log(c);
+};
+let add1 = (a, b, c = 10) => {
+    console.log(a + b);
+    console.log(c);
+};
 let sum = (a, b) => {
     return a + b;
 };
--- src/sandbox.ts
+++ src/sandbox.ts
@@ -4,6 +4,16 @@ let greet = (a:string):void=>{
    console.log(`hello ${a}`)
 };
 
+let add = (a:number,b:number,c?:string):void=>{
+  console.log(a + b)
+  console.log(c)
+};
+
+let add1 = (a:number,b:number,c:number=10):void=>{
+  console.log(a + b)
+  console.log(c)
+}
+
 let sum =(a:number,b:number):number=>{
   return a + b
 };

87e511013f3af54ffbdc0c36749100e8fe1000ce
nelson chege <nelson3chege@gmail.com>
2023-03-29 11:28:03 +0300

    feat:adding function signatures

--- public/sandbox.js
+++ public/sandbox.js
@@ -1,6 +1,21 @@
-var character = "mario";
-console.log(character);
-var inputs = document.querySelectorAll("input");
-inputs.forEach(function (input) {
-    console.log(input);
-});
+"use strict";
+//example 1
+let greet;
+greet = (name) => {
+    console.log(name);
+};
+//example 2
+let cal;
+cal = (first_number, second_number, operation) => {
+    if (operation == "add") {
+        return first_number + second_number;
+    }
+    else {
+        return first_number - second_number;
+    }
+};
+let userAge;
+userAge = (user) => {
+    console.log(`${user.name} is ${user.age} years old`);
+};
+userAge({ name: 'Nelson', age: 30 });
--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,6 +1,28 @@
-const character = "mario";
-console.log(character);
-const inputs = document.querySelectorAll("input");
-inputs.forEach(function (input) {
-  console.log(input);
-});
+
+//example 1
+let greet :(a:string)=>void;
+
+greet =(name:string):void => {
+  console.log(name)
+}
+//example 2
+let cal :(a:number,b:number,c:string)=>number;
+
+cal = (first_number:number, second_number:number,operation:string):number => {
+  if (operation=="add"){
+    return first_number + second_number;
+  }else{
+    return first_number - second_number
+  }
+}
+
+//example 3
+type person = {name:string,age:number}
+
+let userAge: (obj:person)=>void;
+
+userAge =(user:{name:string,age:number}):void => {
+  console.log(`${user.name} is ${user.age} years old`);
+}
+
+userAge({name:'Nelson',age:30})

092d30a27fd784f767f5ad70d421291def972b3c
nelson chege <nelson3chege@gmail.com>
2023-03-29 12:34:22 +0300

    update:adding invoice class

--- public/app.js
+++ public/app.js
@@ -1,6 +1,16 @@
 "use strict";
+class Invoice {
+    constructor(a, b, c, d) {
+        this.finance_type = a;
+        this.name = b;
+        this.details = c;
+        this.amount = d;
+    }
+    format() {
+        return `${this.name}  ${this.finance_type}  for  ${this.details} and ksh${this.amount}`;
+    }
+}
 const form = document.querySelector('.new-item-form');
-// console.log(form.children);
 const finance_type = document.querySelector('#type');
 const toFrom = document.querySelector('#tofrom');
 const Details = document.querySelector('#details');
@@ -8,4 +18,6 @@ const Amount = document.querySelector('#amount');
 form.addEventListener('submit', (e) => {
     e.preventDefault();
     console.log(finance_type.value, toFrom.value, Details.value, Amount.valueAsNumber);
+    const Invoice1 = new Invoice(finance_type.value, toFrom.value, Details.value, Amount.valueAsNumber);
+    console.log(Invoice1.format());
 });
--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,30 @@
+class Invoice {
+
+      finance_type: string;
+      name: string;
+      details: string;
+      amount: number;
+
+      constructor(a:string,b:string,c:string,d:number){
+          this.finance_type = a;
+          this.name = b;
+          this.details = c;
+          this.amount = d;
+      }
+
+      format(){
+        return `${this.name}  ${this.finance_type}  for  ${this.details} and ksh${this.amount}`
+      }
+}
+
 
 
 const form = document.querySelector('.new-item-form') as HTMLFormElement;
-
-// console.log(form.children);
 
 const finance_type = document.querySelector('#type') as HTMLSelectElement;
 const toFrom = document.querySelector('#tofrom') as HTMLInputElement;
 const Details = document.querySelector('#details') as HTMLInputElement;
 const Amount = document.querySelector('#amount') as HTMLInputElement;
-
 
 form.addEventListener('submit',(e:Event)=>{
       e.preventDefault();
@@ -18,5 +34,13 @@ form.addEventListener('submit',(e:Event)=>{
         Details.value,
         Amount.valueAsNumber
       );
+
+      const Invoice1 = new Invoice(finance_type.value,
+        toFrom.value,
+        Details.value,
+        Amount.valueAsNumber)
+      
+      console.log(Invoice1.format());
+      
 })
 

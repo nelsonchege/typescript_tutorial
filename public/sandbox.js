94288204519214e2566a4dff43ade455d1219131
nelson chege <nelson3chege@gmail.com>
2023-03-29 10:46:52 +0300

    feat: creating functions

--- public/sandbox.js
+++ public/sandbox.js
@@ -1,6 +1,9 @@
-var character = "mario";
-console.log(character);
-var inputs = document.querySelectorAll("input");
-inputs.forEach(function (input) {
-    console.log(input);
-});
+"use strict";
+let sample_function;
+let greet = (a) => {
+    console.log(`hello ${a}`);
+};
+let sum = (a, b) => {
+    return a + b;
+};
+let result = sum(4, 5);
--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,6 +1,11 @@
-const character = "mario";
-console.log(character);
-const inputs = document.querySelectorAll("input");
-inputs.forEach(function (input) {
-  console.log(input);
-});
+let sample_function:Function;
+
+let greet = (a:string):void=>{
+   console.log(`hello ${a}`)
+};
+
+let sum =(a:number,b:number):number=>{
+  return a + b
+};
+
+let result =sum(4,5);
